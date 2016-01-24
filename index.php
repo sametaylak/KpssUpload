4be226fda5f3b17a90dee499a3b42983113a43a1
Samet Aylak <sametaylak29@gmail.com>
2016-01-24 05:07:05 +0200

    test

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
 				continue; // Skip invalid file formats
 			}
 	        else{ // No error found! Move uploaded files 
-	        	if (move_uploaded_file($_FILES['files']['tmp_name'][$f], "D:\home\site\wwwroot\uploads\\".$name))
+	        	if (move_uploaded_file($_FILES['files']['tmp_name'][$f], "uploads/".$name))
 				{
 					$file = fopen("uploads/".$name, "rb");
 					$contents = fread($file, filesize("uploads/".$name));
