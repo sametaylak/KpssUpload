31edf16c55e5eea446aaea81c8d44371f1e3ccd4
Samet Aylak <sametaylak29@gmail.com>
2016-01-25 00:30:11 +0200

    test

--- index.php
+++ index.php
@@ -25,3 +25,29 @@
 	</form>
 </body>
 </html>
+<?php
+ob_start();
+session_start();
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+ 
+$kadi = $_POST['kadi'];
+$sifre = $_POST['sifre'];
+ 
+ echo $kadi;
+
+if($kadi=="admin" or $sifre=="kpssnotlar")  {
+    $_SESSION["login"] = "true";
+    header("Location:upload_file.php");
+}
+else {
+    if($kadi=="" or $sifre=="") {
+        echo "<center>Lutfen kullanici adi ya da sifreyi bos birakmayiniz..! <a href=javascript:history.back(-1)>Geri Don</a></center>";
+    }
+    else {
+        echo "<center>Kullanici Adi/Sifre Yanlis.<br><a href=javascript:history.back(-1)>Geri Don</a></center>";
+    }
+}
+ 
+ob_end_flush();
+?>
